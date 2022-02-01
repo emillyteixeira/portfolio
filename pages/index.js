cc607ad246ecfafea667cd0187831d980d753121
Davi Oliveira <emillymirandateixeira@gmail.com>
2022-01-31 22:12:52 -0300

    Adicionando Head e title

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Head from 'next/head'
 import Navbar from '../src/components/Navbar.js';
 import HeroSection from '../src/components/HeroSection.js';
 import Footer from '../src/components/Footer.js';
@@ -6,6 +7,10 @@ import Footer from '../src/components/Footer.js';
 function HomePage() {
   return (
       <>
+      <Head>
+        <title>Portfólio Emilly</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <Navbar />
       <HeroSection/>
       <Footer />
